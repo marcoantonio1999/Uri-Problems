f4b1157211d62b34a4a61ae2e7bc9e51de60884a
marcoantonio1999 <marcoorduna@ciencias.unam.mx>
2021-02-21 22:52:43 -0600

    primeros retos

---
+++ Uri1001/src/Uri1001.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author sasuk
+ */
+public class Uri1001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+       
+       int X = A+B;
+       System.out.println("X = " + X);
+    }
+    
+}
